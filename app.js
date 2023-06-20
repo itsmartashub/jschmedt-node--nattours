4181b35f88e56ae7ac71824ce00e000aedfedf13
itsmartashub <coderawii@gmail.com>
2023-06-20 18:30:58 +0200

    refactor: use app.route() and chain the methods

--- app.js
+++ app.js
@@ -4,40 +4,18 @@ const express = require('express')
 const app = express()
 app.use(express.json()) // middleware je u sustini f-ja koja moze da modifikuje podatke koji nam stizu na server, dakle stoji in the middle of the req i res. Ako ovo zakomentarisemo body je undefined, odn nemamo ga vise
 
-// app.get('/', (req, res) => {
-// 	res.status(200).json({ message: 'Hello World' })
-// })
-// app.post('/', (req, res) => {
-// 	res.send('You can post to this endpoint')
-// })
-
 // podatke ne citamo u app.get u CB, znamo a to opterecuje thread, nema potrebe,
 const tours = JSON.parse(
 	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 )
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
 	res.status(200).json({
 		status: 'success',
 		data: { result: tours.length, tours },
 	})
-})
-
-/* 
-ovo :id moze biti sta god, :var, :x, :p, :c, sta god :D
-
-takodje moze biti i chain tih promenljivih tipa:
-    /api/v1/tours/:id/:name/:level/:c
-
-Vazno, ukoliko client salje req na ovaj path:
-    /api/v1/tours/:id/:name/:level/:c
-on mora isto toliko params da stavi, znaci ne moze:
-    /api/v1/tours/2/nikola
-bude error 404
-
-Ali mozemo za taj parametar da stavimo da je optional, samo dodamo ?
-    /api/v1/tours/:id/:name/:level?/:c?      */
-app.get('/api/v1/tours/:id', (req, res) => {
+}
+const getTour = (req, res) => {
 	console.log(req.params)
 
 	const id = +req.params.id
@@ -55,10 +33,8 @@ app.get('/api/v1/tours/:id', (req, res) => {
 		status: 'success',
 		data: { tour },
 	})
-})
-
-// da bismo dohvatili body podatke u express-u moramo koristiti middleware, to je ono kad napisemo app.use(express.json())
-app.post('/api/v1/tours', (req, res) => {
+}
+const createTour = (req, res) => {
 	// console.log(req.body) // bez middlerware ovo bi bilo undefined
 
 	const newID = tours[tours.length - 1].id + 1
@@ -79,10 +55,8 @@ app.post('/api/v1/tours', (req, res) => {
 		}
 	)
 	// res.send('Done') //! ne smemo dva res da saljemo, i ovaj send i ovaj sa json-om
-})
-
-// Imamo dva HTTPS methoda kojima apdejtujemo podatke: sa PUT ocekujemo da se menja citav objekat, a sa PATCH ocekujemo da se promena vrsi samo nad property-em u tom objektu
-app.patch('/api/v1/tours/:id', (req, res) => {
+}
+const updateTour = (req, res) => {
 	// req.params.id * 1 ovako se takodje pretvara String u Number
 	if (req.params.id * 1 > tours.length) {
 		return res.status(404).json({
@@ -98,9 +72,8 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 			tour: '<Updated tour here...>',
 		},
 	})
-})
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+}
+const deleteTour = (req, res) => {
 	if (req.params.id * 1 > tours.length) {
 		return res.status(404).json({
 			status: 'fail',
@@ -108,12 +81,46 @@ app.delete('/api/v1/tours/:id', (req, res) => {
 		})
 	}
 
-	// ovo je fejk, ne za real world jer nismo zaprravo nista editovali, samo za learning purpose. Inace 204 znaci no content, a to je jer smo ga obrisal
+	// Inace 204 znaci no content, a to je jer smo ga obrisal
 	res.status(204).json({
 		status: 'success',
 		data: null,
 	})
-})
+}
+///////////////////////////////////////////
+/// OVO JE NACIN 1
+///////////////////////////////////////////
+// app.get('/api/v1/tours', getAllTours)
+
+// /*
+// ovo :id moze biti sta god, :var, :x, :p, :c, sta god :D
+
+// takodje moze biti i chain tih promenljivih tipa:
+//     /api/v1/tours/:id/:name/:level/:c
+
+// Vazno, ukoliko client salje req na ovaj path:
+//     /api/v1/tours/:id/:name/:level/:c
+// on mora isto toliko params da stavi, znaci ne moze:
+//     /api/v1/tours/2/nikola
+// bude error 404
+
+// Ali mozemo za taj parametar da stavimo da je optional, samo dodamo ?
+//     /api/v1/tours/:id/:name/:level?/:c?      */
+// app.get('/api/v1/tours/:id', getTour)
+
+// // da bismo dohvatili body podatke u express-u moramo koristiti middleware, to je ono kad napisemo app.use(express.json())
+// app.post('/api/v1/tours', createTour)
+
+// // Imamo dva HTTPS methoda kojima apdejtujemo podatke: sa PUT ocekujemo da se menja citav objekat, a sa PATCH ocekujemo da se promena vrsi samo nad property-em u tom objektu
+// app.patch('/api/v1/tours/:id', updateTour)
+
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+///////////////////////////////////////////
+/// OVO JE NACIN 2 sa app route koji omogucava chainovanje http method-a koji imaju isti http path
+///////////////////////////////////////////
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 const port = 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
