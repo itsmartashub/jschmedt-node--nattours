92da68023eabb1d4399cb2e4c30c18ffbedb242a
itsmartashub <coderawii@gmail.com>
2023-06-29 13:57:21 +0200

    Making the API - Better Aliasing
    DESCRIPTION:
    
    ! tourController.aliasTopTours je MW kojim cemo da dohvatimo 5 najeftinijih tours itema. Naime pre nego sto pozovemo ovaj getAllTours route handler, dodacemo neke fields u query stringu, jer string treba da nam izgleda ovako:
    
        /api/v1/tours?limit=5&sort=ratingAverage,price
    
    sad ce url da izgleda ovako:
        /api/v1/tours/top-5-cheap
    
    ali ce da se ponasa kao da je
        /api/v1/tours?limit=5&sort=ratingAverage,price
    jer cemo u ovom aliasTopTours MW to da setujemo.
    
    Dakle MW koristimo strateski zavisi kako treba da izmenimo req object pre nego sto dodje do servera bas.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,15 @@ exports.checkBody = (req, res, next) => {
 	next() // a sledeci mw je createTour
 }
 */
+
+exports.aliasTopTours = (req, res, next) => {
+	req.query.limit = '5'
+	req.query.sort = '-ratingsAverage,price'
+	req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+
+	next()
+}
+
 exports.getAllTours = async (req, res) => {
 	// console.log(req.requestTime)
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,16 @@ const tourController = require('../controllers/tourController')
 
 const router = express.Router()
 
+/* ! tourController.aliasTopTours je mw kojim cemo da dohvatimo 5 najeftinijih itema. naime pre nego sto pozovemo ovaj getAllTours route handler, dodacemo neke fields u query stringu, jer string treba da nam izgleda ovako:
+``` /api/v1/tours?limit=5&sort=ratingAverage,price
+sad ce url da izgleda ovako:
+``` /api/v1/tours/top-5-cheap
+al ce da se ponasa kao da je /api/v1/tours?limit=5&sort=ratingAverage,price, jer cemo u ovom aliasTopTours mw to da setujemo.
+dakle mw koristimo strateski zavisi kako treba da izmenimo req object pre nego sto dodje do servera bas */
+router
+	.route('/top-5-cheap')
+	.get(tourController.aliasTopTours, tourController.getAllTours)
+
 /*
 sad cemo da napisemo middleware koji ce da se okine samo kada je ovaj dole :id prisutan u url-u. Param mw ima 4 parametara. Elem, ovaj mw se obvio nece pozvati kada se polsaje req ka /api/v1/users/:id, je je ova mwf-ja specificirana/namenjena samo za tour router. Jonas je dao analogiju da je koriscenje svaki ruter kao neka mini sub aplikacija, jedna za svaki resource */
 // router.param('id', tourController.checkID)
