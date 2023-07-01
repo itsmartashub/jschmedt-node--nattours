7093b7e90e979b2a063a78fbb327c4a305e8f0d5
itsmartashub <coderawii@gmail.com>
2023-07-01 16:58:38 +0200

    feat(virtauls): Virtual Properties
    DESCRIPTION
    
    * durationWeeks - ime virtuelnog propertija. I onda definisegemo get method,a to je jer se ovaj virtual property kreira svaki x kada dohvatimo neke podatke iz db. get() prima za argument cb f-ju, i to NE ARROW vec KLASICNU jer ona ima svoj this keyword, a to nam je ovde potrebno jer ce ovde this ukazivati na trenutni document.
    ! Ovaj durationWeeks virtaul propety ne mozemo koristiti u query-ima, jer on tehnicki nije deo db-a. Tipa NE MOZEMO da sacuvamo:
    ``` let query = Tour.find().where('durationWeeks').equals(1)
    I posto je ovo vise deo biznis logike, a ne app, ovo radimo u tourModel.js, a ne tourController.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,86 +1,103 @@
 const mongoose = require('mongoose')
 
-const tourSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'A tour must have a name'],
-		unique: true,
-		trim: true, // samo radi na stringovima, uklanja sve whitespaces
-		// maxlength: [
-		// 	40,
-		// 	'A tour name must have less or equal then 40 characters',
-		// ],
-		// minlength: [
-		// 	10,
-		// 	'A tour name must have more or equal then 10 characters',
-		// ],
-		// validate: [validator.isAlpha, 'Tour name must only contain characters']
-	},
-	slug: String,
-	duration: {
-		type: Number,
-		required: [true, 'A tour must have a duration'],
-	},
-	maxGroupSize: {
-		type: Number,
-		required: [true, 'A tour must have a group size'],
-	},
-	difficulty: {
-		type: String,
-		required: [true, 'A tour must have a difficulty'],
-		// enum: {
-		// 	values: ['easy', 'medium', 'difficult'],
-		// 	message: 'Difficulty is either: easy, medium, difficult',
+const tourSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'A tour must have a name'],
+			unique: true,
+			trim: true, // samo radi na stringovima, uklanja sve whitespaces
+			// maxlength: [
+			// 	40,
+			// 	'A tour name must have less or equal then 40 characters',
+			// ],
+			// minlength: [
+			// 	10,
+			// 	'A tour name must have more or equal then 10 characters',
+			// ],
+			// validate: [validator.isAlpha, 'Tour name must only contain characters']
+		},
+		slug: String,
+		duration: {
+			type: Number,
+			required: [true, 'A tour must have a duration'],
+		},
+		maxGroupSize: {
+			type: Number,
+			required: [true, 'A tour must have a group size'],
+		},
+		difficulty: {
+			type: String,
+			required: [true, 'A tour must have a difficulty'],
+			// enum: {
+			// 	values: ['easy', 'medium', 'difficult'],
+			// 	message: 'Difficulty is either: easy, medium, difficult',
+			// },
+		},
+		ratingsAverage: {
+			type: Number,
+			default: 4.5,
+			// min: [1, 'Rating must be above 1.0'],
+			// max: [5, 'Rating must be below 5.0'],
+		},
+		ratingsQuantity: {
+			type: Number,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			required: [true, 'A tour must have a price'],
+		},
+		priceDiscount: {
+			type: Number,
+			// validate: {
+			// 	validator: function (val) {
+			// 		// this only points to current doc on NEW document creation
+			// 		return val < this.price
+			// 	},
+			// 	message: 'Discount price ({VALUE}) should be below regular price',
+			// },
+		},
+		summary: {
+			type: String,
+			trim: true,
+			required: [true, 'A tour must have a description'],
+		},
+		description: {
+			type: String,
+			trim: true,
+		},
+		imageCover: {
+			type: String, // ime slike. dakle ne ucitvamo sliku u db, jer je to losa praksa, slike budu negde u filesystem, pa ucitavamo odatle, a u db cuvamo samo ime slike
+			required: [true, 'A tour must have a cover image'],
+		},
+		images: [String], // po type je ovo niz stringova, dakle niz u kom su stringovi/nazivi slika
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+			select: false, //! da excludujemo field, recimo ovo je bitno za passworde, njih nikad necemo da includujemo klijentu
+		},
+		startDates: [Date],
+		// secretTour: {
+		// 	type: Boolean,
+		// 	default: false,
 		// },
 	},
-	ratingsAverage: {
-		type: Number,
-		default: 4.5,
-		// min: [1, 'Rating must be above 1.0'],
-		// max: [5, 'Rating must be below 5.0'],
-	},
-	ratingsQuantity: {
-		type: Number,
-		default: 0,
-	},
-	price: {
-		type: Number,
-		required: [true, 'A tour must have a price'],
-	},
-	priceDiscount: {
-		type: Number,
-		// validate: {
-		// 	validator: function (val) {
-		// 		// this only points to current doc on NEW document creation
-		// 		return val < this.price
-		// 	},
-		// 	message: 'Discount price ({VALUE}) should be below regular price',
-		// },
-	},
-	summary: {
-		type: String,
-		trim: true,
-		required: [true, 'A tour must have a description'],
-	},
-	description: {
-		type: String,
-		trim: true,
-	},
-	imageCover: {
-		type: String, // ime slike. dakle ne ucitvamo sliku u db, jer je to losa praksa, slike budu negde u filesystem, pa ucitavamo odatle, a u db cuvamo samo ime slike
-		required: [true, 'A tour must have a cover image'],
-	},
-	images: [String], // po type je ovo niz stringova, dakle niz u kom su stringovi/nazivi slika
-	createdAt: {
-		type: Date,
-		default: Date.now(),
-		select: false, //! da excludujemo field, recimo ovo je bitno za passworde, njih nikad necemo da includujemo klijentu
-	},
-	startDates: [Date],
-	// secretTour: {
-	// 	type: Boolean,
-	// 	default: false,
-	// },
+
+	//? SCHEMA OPTIONS
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+)
+
+/* ? VIRTUALS
+* durationWeeks - ime virtuelnog propertija. I onda definisegemo get method,a to je jer se ovaj virtual property kreira svaki x kada dohvatimo neke podatke iz db. get() prima za argument cb f-ju, i to NE ARROW vec KLASICNU jer ona ima svoj this keyword, a to nam je ovde potrebno jer ce ovde this ukazivati na trenutni document.
+! Ovaj durationWeeks virtaul propety ne mozemo koristiti u query-ima, jer on tehnicki nije deo db-a. Tipa NE MOZEMO da sacuvamo:
+``` let query = Tour.find().where('durationWeeks').equals(1)
+I posto je ovo vise deo biznis logike, a ne app, ovo radimo u tourModel.js, a ne tourController.js */
+tourSchema.virtual('durationWeeks').get(function () {
+	return this.duration / 7
 })
 
 const Tour = mongoose.model('Tour', tourSchema) // obicaj je da se koristi uppercase, prvi argument je ime modela, a drugi je shema
