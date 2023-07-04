53179b92b9de1f1f150592dca8f10d19215776b9
itsmartashub <coderawii@gmail.com>
2023-07-04 18:16:59 +0200

    Errors During Development vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,43 @@
+const sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		error: err,
+		message: err.message,
+		stack: err.stack,
+	})
+}
+const sendErrorProd = (err, res) => {
+	//? Operatioanal, trusted error: send message to client
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+		})
+
+		//? Programming or other uknown error: dont leak error details
+	} else {
+		// 1) Log error
+		console.log('ERROR 💥 ', err)
+
+		// 2) Send generic message
+		res.status(500).json({
+			status: 'error',
+			message: 'Something went wrong!',
+		})
+	}
+}
+
 module.exports = (err, req, res, next) => {
 	// console.log(err.stack)
 
 	err.statusCode = err.statusCode || 500
 	err.status = err.status || 'error'
+
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev()
+	} else if (process.env.NODE_ENV === 'production') {
+		sendErrorProd()
+	}
 
 	res.status(err.statusCode).json({
 		status: err.status,
