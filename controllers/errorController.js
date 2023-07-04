b743a602427c9a435801de38b3b4604d15926468
itsmartashub <coderawii@gmail.com>
2023-07-04 20:18:33 +0200

    Handling Invalid Databae IDs
    DESCRIPTION
        - meni ne radi kako je expected. Error mi nije renderovan na request kao njemu. Nece ni da registruje konzolu za:
    
        if (process.env.NODE_ENV === 'development') {
    		console.log('DEVELOPMENT')
    	} else if (process.env.NODE_ENV === 'production') {
    		console.log('PRODUCTION')
        }

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError')
+
+const handleCastErrorDB = (err) => {
+	const message = `Invalid ${err.path}: ${err.value}.`
+	return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
 		status: err.status,
@@ -34,9 +41,19 @@ module.exports = (err, req, res, next) => {
 	err.status = err.status || 'error'
 
 	if (process.env.NODE_ENV === 'development') {
-		sendErrorDev()
+		console.log('DEVELOPMENT')
+
+		sendErrorDev(err, res)
 	} else if (process.env.NODE_ENV === 'production') {
-		sendErrorProd()
+		console.log('PRODUCTION')
+
+		let error = { ...err }
+
+		if (error.name === 'CastError') error = handleCastErrorDB(error)
+
+		sendErrorProd(error, res)
+	} else {
+		console.log(typeof process.env.NODE_ENV)
 	}
 
 	res.status(err.statusCode).json({
