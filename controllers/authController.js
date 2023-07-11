8b2fe9c8dc392208619eecf69758ff46aaa78a50
itsmartashub <coderawii@gmail.com>
2023-07-11 20:22:55 +0200

    feat(auth): Signing up Users (jwt)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,19 @@
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const newUser = await User.create(req.body)
+	// const newUser = await User.create(req.body) //! BIG SECURITY FLOW, nikako ovako ne sme da se kreira korisnik
 
-	res.status(201).json({ status: 'success', data: { user: newUser } })
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	})
+
+	const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	})
+	res.status(201).json({ status: 'success', token, data: { user: newUser } })
 })
