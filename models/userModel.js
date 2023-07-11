3d9b3eec556dfc419bc5d3da7c844576c4a267b8
itsmartashub <coderawii@gmail.com>
2023-07-11 19:31:02 +0200

    feat(auth): Managing Password (bcryptjs)

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 // name, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -24,7 +25,33 @@ const userSchema = new mongoose.Schema({
 	passwordConfirm: {
 		type: String,
 		required: [true, 'Please confirm your password'],
+		validate: {
+			// This only works on CREATE and SAVE!!!
+			validator: function (el) {
+				// validator fn vraca true ili false, true znaci da nema error, false da ima
+				return el === this.password
+			},
+			message: 'Passwords are not the same',
+		},
 	},
+})
+
+/* 
+Razlog zasto ovo ce se enkripcija desiti pre nego sto primimo user podatke i uupisemo ih u bazu. Ovde zelimo da enkriptujemo password samo ako je password field apdejtovano tj kada je password tek kreiran ili apdejtovvan postojeci */
+userSchema.pre('save', async function (next) {
+	// ? Only run this fn if password was actuaally modified
+	if (!this.isModified('password')) return next()
+
+	/*
+	this.password je current password u db.
+	Ovaj drugi argument u hash() metodul je koliko jaku enkripciju kor, kor smo nekada 8, pa 10, a sad mozemo 12 jer su danas kompovi jaci, veci broj vise CPU-a vuce, ali je i password jaci */
+	// ? Hash the password with cost of 10
+	this.password = await bcrypt.hash(this.password, 12)
+
+	// ? Delete passwordConfirm field
+	this.passwordConfirm = undefined // brisemo confirm password, jer nam on treba samo za validaciju, dakle ne treba nigde u bazi posle de ga cuvamo, to sto je required znaci da je required prilikom inputa, a ne u db-u
+
+	next()
 })
 
 const User = mongoose.model('User', userSchema)
