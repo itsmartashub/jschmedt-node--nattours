0cf0cc2713d63210bce0442b8ae8d00e2f900d05
itsmartashub <coderawii@gmail.com>
2023-08-14 15:45:32 +0200

    feat(modelling/reviews): populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,6 +35,18 @@ const reviewSchema = new mongoose.Schema(
 	}
 )
 
+//! Kada zelimo da populate-mo recimo 2 polja, moramo pozvati 2 populate(), dakle za svako polje jedan populate
+reviewSchema.pre(/^find/, function (next) {
+	//! imaj na umu da ce ovo dodati extra queries, jer u pozadini mongoose salje plus 2 query-a, 1 za tour i 1 za user ne bi li pronasao matching document. Dakle i vremena ce trebati vise!!
+	this.populate({
+		path: 'tour',
+		select: 'name', // zelimo samo tour name i nista vise
+	}).populate({
+		path: 'user',
+		select: 'name photo', // zelimo user name i user photo. ne zelimo da leakujemo sve podatke o useru ako recimo on ostavi review, tipa email, itd, vec zelimo da svaki review ima samo ime i sliku korisnika koji je ostavio review
+	})
+	next()
+})
 const Review = mongoose.model('Review', reviewSchema)
 
 module.exports = Review
